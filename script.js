9f23707ca8e0a3fa5c0653a9534c982bffcb9771
Aleksey <noiselim@gmail.com>
2020-08-22 22:51:54 +0300

    add fun_writeYourGenres, showMyDB

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-const number0Films = +prompt('Сколько фильмов вы уже посмотрели?','');
+let number0Films;
+
+function start() {
+    number0Films = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+    while (number0Films == '' || number0Films == null || isNaN(number0Films)) {
+        number0Films = +prompt('Сколько фильмов вы уже посмотрели?','');
+    }
+}
+// start();
 
 const personalMovieDB = {
     count: number0Films,
@@ -10,32 +19,67 @@ const personalMovieDB = {
     privat: false
 };
 
-
-
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените фильм?', '');
-    
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    }
-    else {
-          i--;
-          console.log('error');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените фильм?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        }
+        else {
+              i--;
+              console.log('error');
+        }
     }
 }
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    console.log('Вы классный парень');
-} else if (personalMovieDB.count > 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка')
+
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('Вы классный парень');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка')
+    }
 }
 
+// detectPersonalLevel();
 
-console.log(personalMovieDB);
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
 
+
+// function writeYourGenres() {
+//     for (let i = 1; i <= 3; i++) {
+//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр №${i}`);
+//     }
+// }
+// writeYourGenres();
+
+let questionsOfGenres;
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        questionsOfGenres = prompt(`Ваш любимый жанр №${i}`);
+        
+        if (questionsOfGenres != null && questionsOfGenres != '' && questionsOfGenres.length < 50) {
+            personalMovieDB.genres[i - 1] = questionsOfGenres;
+        }
+        else {
+            i--;
+            console.log('error');
+        }
+    }
+}
+
+writeYourGenres();
--- script2.js
+++ script2.js
@@ -1,3 +1,5 @@
+"use strict"
+
 if (4 == 8) {
     console.log('OK!');
 } else {
@@ -63,3 +65,77 @@ for (let i = 1; i < 10; i++) {
     }
     console.log(i);
 }
+
+// ///////////Функции///////////////
+
+// function declaration (работает до того как будет объявлена)
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+}
+showFirstMessage('Hello world');
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(5, 7));
+
+function ret() {
+    let num = 30;
+    return = num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+// ////////////function expression
+// создается только тогда, когда до нее доходит поток кода, ее
+// можно вызвать только после объявления
+
+const logger = function() {
+    console.log('hello');
+};
+
+logger();
+
+// Стрелочная функция
+
+const calc = (a, b) => a + b;
+const calc2 = (a, b) => {return a + b};
+const calc2 = (a, b) => {
+    console.log('1');
+    return a + b;
+};
+
+console.log(calc(3, 2));
+
+// Методы и свойства строк и чисел///////////////////////
+
+const str = 'test';
+const arr = [1, 2, 3];
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+// console.log(fruit.indexOf('fruit'));
+console.log(fruit.indexOf('i'));
+
+const logg = 'Hello world';
+
+console.log(logg.slice(6, 11));
+console.log(logg.slice(6));
+console.log(logg.slice(-5, -1));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5)); // Второй аргумент говорит сколько символов вырезать
+
+const num = 12.49;
+console.log(Math.round(num)); // 12
+
+const test = '12.2px';
+console.log(parseInt(test)); // 12
+console.log(parseFloat(test)); // 12.2
