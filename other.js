229787a9256e3e14faded355ba3f5436231c082a
noiseLim <noiselim@gmail.com>
2020-08-28 17:54:52 +0300

    add practice

--- other.js
+++ other.js
@@ -9,3 +9,26 @@ let obj = {
 let arr = [1, 2, 3];
 
 console.log(Array.isArray(arr));
+
+
+
+function Car(make, model, year, owner) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.owner = owner;
+  }
+  
+// var mycar = new Car("Eagle", "Talon TSi", 1993, rand);
+// console.log(mycar);
+
+function Person(name, age, sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+}
+
+var rand = new Person('John', 23, 'M');
+
+var mycar = new Car("Eagle", "Talon TSi", 1993, rand);
+console.log(mycar);
