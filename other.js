dd1af6719e1585b1731a9cb6cd6980b280a8b2d2
noiseLim <noiselim@gmail.com>
2020-08-28 17:27:29 +0300

    add files 'other.js'

---
+++ other.js
@@ -0,0 +1,11 @@
+// Метод Array.isArray() возвращает true, если объект является массивом и 
+// false, если он массивом не является.
+
+let obj = {
+    a: 1,
+    b: 2,
+};
+
+let arr = [1, 2, 3];
+
+console.log(Array.isArray(arr));
