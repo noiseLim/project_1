9eb41c09dd96d12da8d2435e2323741f3ef5f075
noiseLIm <noiselim@gmail.com>
2020-09-24 11:47:28 +0300

    finish lesson #39. setTimeout, setInterval.

--- 39. setTimeout/script.js
+++ 39. setTimeout/script.js
@@ -26,20 +26,52 @@ const btn = document.querySelector('.btn');
 let timerId,
     i = 0;
 
-btn.addEventListener('click', () => {
-    // const timeId = setTimeout(logger, 2000);
-    timerId = setInterval(logger, 2000); //использует интервал
-});
+// btn.addEventListener('click', () => {
+//     const timeId = setTimeout(logger, 2000);
+//     timerId = setInterval(logger, 500); //использует интервал
+// });
 
-clearInterval(timerId); 
+// clearInterval(timerId); 
 /*переменная находится локано и не может быть использована
 решаем проблему объявив её глобально ничего ей не присваивая
 после клика в неё записывается значение (перезаписав переменную)*/
 
-function logger () {
-    if (i === 3) {
-        clearInterval(timerId);
+// function logger () {
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('Hello');
+//     i++;
+// } 
+/* в случаае если в logger описана тяжелая функция для выполнения 
+которой, например нужно 3 секунды, скрипт сработает так - выполняется 
+задержка 500 мс, потом скрипт 3 сек., после чего прописанная задержка
+500 мс не выполнится, она будет поглощена долгим скриптом*/
+
+//чтобы этого избежать используем таймер внутри таймера
+// let id = setTimeout (function log () {
+//     console.log('Hello');
+//     id = setTimeout (log, 500);
+// }, 500);
+/* в таком случае таймер 500 мс будет запускаться строго после выполнения
+функции, в независимости от времени её выполнения*/
+
+//пример анимации на js
+function myAnimation () {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+    function frame () {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+
     }
-    console.log('Hello');
-    i++;
 }
+
+btn.addEventListener('click', myAnimation);
