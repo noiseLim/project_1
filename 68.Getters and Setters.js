7de7cb5ea6c5812b2e3b19074ebe535a9300a680
noiseLIm <noiselim@gmail.com>
2020-10-19 16:01:15 +0300

    add lesson #68. Getters and Setters

---
+++ 68.Getters and Setters.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*объекты состоят из свойств и методов
+свойства (данных) - это то, что описывает наш объект 
+(характеристики, данные)
+
+методы - это то, что умеет делать наш объект*/
+
+// Второй вид свойств объекта акцессоры
+// Getter - позволяет получать значение
+// Setter - позволяет устанавливать значение
+
+const persone = {
+    name: 'Aleksey',
+    age: 25,
+
+    get userAge() {
+        return this.age;
+    },
+
+    set userAge(num) {
+        this.age = num;
+    }
+};
+console.log(persone.userAge = 33); /*без сеттера не сможем утановить
+новое значение*/
+console.log(persone.userAge);
+
+console.log(persone);
